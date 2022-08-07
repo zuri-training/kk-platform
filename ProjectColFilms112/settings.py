6604dcaafbdaf6cd91d3e50c20458af0f11646c2
Ibrahim <ibrahimmuhammad018@gmail.com>
2022-08-07 13:15:03 +0000

    Created License Adding DB-SCHEMA png Hiding SECRET KEY Adding project requiremnt file Updating README.md Changes to be committed: 	new file:   License 	new file:   Proj-112-db-schema.drawio.png 	modified:   ProjectColFilms112/settings.py 	modified:   README.md 	new file:   requirements.txt

--- ProjectColFilms112/settings.py
+++ ProjectColFilms112/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
+from django import conf
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@za7c@p5c8zujs7$v_tofar_06md)g)^9dq8rxghe4d%(j&2+9'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userapp',
+    
 ]
 
 MIDDLEWARE = [
