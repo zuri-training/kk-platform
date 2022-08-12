b68e068c0953e99d439ae66b5cf6c29a8a330011
videlleudeh <chizzyudeh03@gmail.com>
2022-08-12 20:56:50 +0100

    Update settings.py

--- ProjectColFilms112/settings.py
+++ ProjectColFilms112/settings.py
@@ -79,17 +79,17 @@ WSGI_APPLICATION = 'ProjectColFilms112.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'kampuskonnect',
-#         'USER': 'postgres',
-#         'PASSWORD': 'team112',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kampuskonnect',
+        'USER': 'postgres',
+        'PASSWORD': 'team112',
+        'HOST': 'localhost',
+        'PORT': '5432',
 
-#     }
-# }
+    }
+}
 
 DATABASES = {
     'default': {
