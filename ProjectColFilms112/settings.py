3fe8a297bb7beab2696fb4e37a94a1453d244d69
Adeboga Abigail <bogadeji@gmail.com>
2022-08-15 19:42:38 +0100

    Fix collectstatic error

--- ProjectColFilms112/settings.py
+++ ProjectColFilms112/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
-DEBUG = FALSE
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
